646d5b6c9e6f464d6c2df127be5ef0d4fba7d549
yixingw <wangyixingvictor@gmail.com>
2018-03-08 11:59:06 -0600

    train_test data resplit

--- text_CNN/get_train_test_data.py
+++ text_CNN/get_train_test_data.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import copy
+from sklearn.model_selection import train_test_split
 
 def get_data(train_data_path,test_data_path):
 	train_X = np.load(train_data_path)
@@ -8,14 +9,14 @@ def get_data(train_data_path,test_data_path):
 	for i in range(12500):
 		train_Y[i] = 1
 	test_Y = copy.deepcopy(train_Y)
-	#shuffle the data 
-	s = np.arange(train_X.shape[0])
-	np.random.shuffle(s)
-	train_X = train_X[s]
-	train_Y = train_Y[s]
-	test_X = test_X[s]
-	test_Y = test_Y[s]
-	return train_X,train_Y,test_X,test_Y
+	#concate the train and text data
+	data_X = np.concatenate((train_X,test_X),axis = 0)
+	data_Y = np.concatenate((train_Y,test_Y),axis = 0)
+
+	#split the data 
+	X_train, X_test, y_train, y_test = train_test_split(data_X, data_Y, test_size=0.2, random_state=42)
+
+	return X_train,y_train,X_test,y_test
 	
 if __name__ == '__main__':
 	train_X,train_Y,test_X,test_Y = get_data('train_id_mat.npy','test_id_mat.npy')
